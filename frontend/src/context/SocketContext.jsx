6e3984afdba7153e249fa690a24a6d5448a41ea3
Corey Lee <clee66909@gmail.com>
2024-06-06 13:22:38 -0700

    Deploying website

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,8 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:3000", {
+            // const socket = io("http://localhost:3000", {
+            const socket = io("https://instant-message-clone.onrender.com/", {
                 query: {
                     userId: authUser._id,
                 },
